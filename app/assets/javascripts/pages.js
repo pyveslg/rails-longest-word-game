8d2c1004f9dfb87cc6322508256e1d459aa8fac1
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-05-15 15:41:29 +0200

    Core game implementation

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,12 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// $(document).ready(function(){
+
+//   $("#submit").click(function(e){
+//     e.preventDefault();
+//     var name = Time.now
+//     $("#time").attr("name", #{<%= Time.now %>});
+//   });
+// });
